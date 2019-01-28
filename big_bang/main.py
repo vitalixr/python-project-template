ec0dfc3bcd698f9e9c64d968ad38aafaa64f1823
Dixie Flatline <vitalisbox@gmail.com>
2019-01-28 22:47:43 +0300

    Better cli template with initialization of loggin system

--- big_bang/main.py
+++ big_bang/main.py
@@ -1,16 +1,30 @@
 import sys
+import logging
+from pprint import pprint
 
 import click
 
 
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+
 @click.group()
-def cli():
-    pass
+@click.option('--debug', '-d', is_flag=True, default=False)
+@click.pass_context
+def cli(ctx, debug):
+    if debug:
+        logger.setLevel(logging.DEBUG)
 
 
 @cli.command()
 @click.option('--name', help='The name.')
-def hello(name):
+@click.pass_context
+def hello(ctx, name):
     click.echo('Hello %s!' % name)
 
 
